91635994de2db24238be3fa8da7e66adf014912d
Matija Novosel <mnovosel5@gmail.com>
2024-04-09 11:23:34 +0200

    New icons

--- src/components/logoHeader/LogoHeader.vue
+++ src/components/logoHeader/LogoHeader.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="text-center text-h1 text-orange mb-5">🐈</div>
-  <div class="text-center text-h4 text-orange mb-2 font-weight-bold">Neat memos</div>
+  <div class="d-flex flex-column align-center">
+    <img
+      src="/cat.png"
+      class="mb-3"
+      width="130"
+    />
+    <div class="text-center text-h4 text-orange mb-2 font-weight-bold">Neat memos</div>
+  </div>
 </template>
 
 <script setup lang="ts">
