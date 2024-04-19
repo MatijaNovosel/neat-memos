893f8d33473d42e482edac2b904da77ac6dcdd7a
Matija Novosel <mnovosel5@gmail.com>
2024-04-19 11:53:29 +0200

    Added expanded md table

--- src/components/markdownRenderer/MarkdownRenderer.vue
+++ src/components/markdownRenderer/MarkdownRenderer.vue
@@ -8,9 +8,13 @@
 <script setup lang="ts">
 import MarkdownIt from "markdown-it";
 import MarkdownItHighlightjs from "markdown-it-highlightjs";
+import MarkdownItMultiMdTable from "markdown-it-multimd-table";
 import MarkdownItTasklists from "markdown-it-task-lists";
 
-const markdown = new MarkdownIt().use(MarkdownItHighlightjs).use(MarkdownItTasklists);
+const markdown = new MarkdownIt()
+  .use(MarkdownItHighlightjs)
+  .use(MarkdownItTasklists)
+  .use(MarkdownItMultiMdTable);
 
 const props = defineProps<{
   source: string;
